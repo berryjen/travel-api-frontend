c11c68bc7196a676d374cd56a48a98a5afb1d290
Jen <73888356+berryjen@users.noreply.github.com>
2024-06-16 17:42:17 +0100

    created React form for recording user visit input

--- src/components/new-visit.jsx
+++ src/components/new-visit.jsx
@@ -1,25 +1,32 @@
 import { useState } from "react";
 export default function NewVisit({ userName }) {
-  const [inputValue, setInputValue] = useState("");
-  const [countryValue, setCountryValue] = useState("");
-
+  const [formData, setFormData] = useState({
+    country_id: "",
+    arrival_time: "",
+    departure_time: "",
+  });
   const handleChange = (event) => {
-    setCountryValue(event.target.value);
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
   };
-
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
   return (
     <>
       <h1>Hi {userName}, you have travelled to the following places:</h1>
       <ul></ul>
-      <form>
-        <label>
-          Input Value:
-          <input type="text" value={inputValue} onChange={handleChange} />
-        </label>
-        <p>Input Value: {inputValue}</p>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="country">
           Enter country:{" "}
-          <input type="text" id="country" name="country_id" required />{" "}
+          <input
+            type="text"
+            id="country"
+            name="country_id"
+            required
+            value={formData.name}
+            onChange={handleChange}
+          />
         </label>
         <label htmlFor="arrivalTime">
           Enter arrival time:
@@ -28,6 +35,8 @@ export default function NewVisit({ userName }) {
             id="arrivalTime"
             name="arrival_time"
             required
+            value={formData.name}
+            onChange={handleChange}
           />
         </label>
         <label htmlFor="departureTime">
@@ -37,6 +46,8 @@ export default function NewVisit({ userName }) {
             id="departureTime"
             name="departure_time"
             required
+            value={formData.name}
+            onChange={handleChange}
           />
         </label>
 
