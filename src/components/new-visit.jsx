28a8b0c24a436a95bce09daaa4b7417b3e38e3e0
Jen <jennifer.cc.su@gmail.com>
2026-01-27 12:35:32 +0800

    fix: comment out awesomplete import.

--- src/components/new-visit.jsx
+++ src/components/new-visit.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import LogoutButton from "./logout-button";
 import CountriesAutocomplete from "./countries-autocompletion";
-import awesomplete from "awesomplete";
+// import awesomplete from "awesomplete";
 
 export default function NewVisit({ userName, setUserName }) {
   console.log('new visit user name', userName, setUserName);
